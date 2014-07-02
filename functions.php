00deecb7473d80fa631fa1a8e36de6cf8ba33cff
Chris Klosowski <cklosowski@gmail.com>
2014-07-01 22:46:18 -0700

    Adding Google Analtyics

--- functions.php
+++ functions.php
@@ -4,6 +4,22 @@ function rah_remove_actions() {
 	remove_action( 'interface_after_loop_content', 'interface_next_previous', 5 );
 }
 
+add_action( 'interface_before', 'rah_add_ga', 1 );
+function rah_add_ga() {
+	?>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-2194977-40', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+	<?
+}
+
 add_action( 'interface_after_loop_content', 'rah_interface_next_previous', 5 );
 
 function rah_interface_next_previous() {
