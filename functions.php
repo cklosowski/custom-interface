b7f680ac1158da760a37f59ed6c5d34481c96693
Chris Klosowski <cklosowski@gmail.com>
2014-07-02 19:16:05 -0700

    Update GA Tracking code

--- functions.php
+++ functions.php
@@ -7,16 +7,16 @@ function rah_remove_actions() {
 add_action( 'interface_before', 'rah_add_ga', 1 );
 function rah_add_ga() {
 	?>
-	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	  ga('create', 'UA-2194977-40', 'auto');
-	  ga('send', 'pageview');
+  ga('create', 'UA-2194977-40', 'auto');
+  ga('send', 'pageview');
 
-	</script>
+</script>
 	<?
 }
 
