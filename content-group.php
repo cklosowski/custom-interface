831c13b265dc37ef78566779113c1e8fa713a051
Chris Klosowski <cklosowski@gmail.com>
2015-06-08 01:25:47 -0700

    Adding support for location

--- content-group.php
+++ content-group.php
@@ -9,137 +9,150 @@
 ?>
 <?php
    /**
-    * interface_before_primary
-    */
+	* interface_before_primary
+	*/
    do_action( 'interface_before_primary' );
 ?>
 <div id="primary" class="no-margin-left">
   <?php
-      /**
-       * interface_before_loop_content
-     *
-     * HOOKED_FUNCTION_NAME PRIORITY
-     *
-     * interface_loop_before 10
-       */
-      do_action( 'interface_before_loop_content' );
+	  /**
+	   * interface_before_loop_content
+	 *
+	 * HOOKED_FUNCTION_NAME PRIORITY
+	 *
+	 * interface_loop_before 10
+	   */
+	  do_action( 'interface_before_loop_content' );
 
-      /**
-       * interface_loop_content
-     *
-     * HOOKED_FUNCTION_NAME PRIORITY
-     *
-     * interface_theloop 10
-       */
+	  /**
+	   * interface_loop_content
+	 *
+	 * HOOKED_FUNCTION_NAME PRIORITY
+	 *
+	 * interface_theloop 10
+	   */
   global $post;
 
   if( have_posts() ) {
-    while( have_posts() ) {
-      the_post();
+	while( have_posts() ) {
+	  the_post();
 
-      $group_image = get_post_meta( $post->ID, '_rah_group_fb_icon', true );
-      $hosts = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'hosts', 'posts_per_page' => -1, 'post_status' => 'publish' ), ARRAY_A );
-      $host_count = is_array( $hosts ) ? count( $hosts ) : 0;
-      $group_stats = rah_get_group_rating_stats( $post->ID );
-      do_action( 'interface_before_post' );
-      ?>
-      <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <article>
-          <header class="entry-header">
-            <?php if(get_the_time( get_option( 'date_format' ) )) { ?>
+	  $group_image = get_post_meta( $post->ID, '_rah_group_fb_icon', true );
+	  $hosts = get_children( array( 'post_parent' => $post->ID, 'post_type' => 'hosts', 'posts_per_page' => -1, 'post_status' => 'publish' ), ARRAY_A );
+	  $host_count = is_array( $hosts ) ? count( $hosts ) : 0;
+	  $group_stats = rah_get_group_rating_stats( $post->ID );
+	  do_action( 'interface_before_post' );
+	  ?>
+	  <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<article>
+		  <header class="entry-header">
+			<?php if(get_the_time( get_option( 'date_format' ) )) { ?>
 
-            <h1 class="entry-title">
-              <?php if( $group_image ) :?><img src="<?php echo $group_image; ?>" /> &nbsp;<?php endif; ?><?php the_title();?>
-              <?php if( $group_stats['group_rating'] > 0 ) :?>
-                <div class="group-title-ratings"><?php echo rah_generate_stars( $group_stats['group_rating'] ); ?></div>
-              <?php endif; ?>
-            </h1>
-            <!-- .entry-title -->
-            <div class="entry-meta clearfix">
-              <div class="date"><span>
-                Added On: <?php the_time( get_option( 'date_format' ) ); ?>
-                </span></div>
-              <div class="by-author"><a href="<?php the_permalink(); ?>">
-                <?php printf( _n( '%d Host', '%d Hosts', $host_count, 'interface' ), $host_count ); ?>
-              </a></div>
-              <?php if( $group_stats['group_reviews'] > 0 ) :?>
-              <div class="comments"><span>Host Reviews: <?php echo $group_stats['group_reviews']; ?></span></div>
-              <?php endif; ?>
-            </div>
-            <!-- .entry-meta -->
-          </header>
-          <!-- .entry-header -->
-          <?php } ?>
-          <div class="entry-content clearfix">
-            <?php the_content(); ?>
-            <h5>Hosts</h5>
-            <?php foreach ( $hosts as $host ) : ?>
-            <div>
-              <a href="<?php echo get_permalink( $host['ID'] ); ?>">
-                <div class="archive-avatar">
-                  <?php echo get_avatar( get_user_id_from_host_id( $host['ID'] ) ); ?>
-                  <p>
-                    <div><?php echo $host['post_title']; ?></div>
-                    <div class="star-ratings-wrapper">
-                      <?php echo rah_generate_stars( get_post_meta( $host['ID'], '_host_rating', true ) ); ?>
-                    </div>
-                  </p>
-                </div>
-              </a>
-            </div>
-            <?php endforeach; ?>
-          </div>
-          <?php if(get_the_time( get_option( 'date_format' ) )) { ?>
-        </header>
-        <?php } ?>
+			<h1 class="entry-title">
+			  <?php if( $group_image ) :?><img src="<?php echo $group_image; ?>" /> &nbsp;<?php endif; ?><?php the_title();?>
+			  <?php if( $group_stats['group_rating'] > 0 ) :?>
+				<div class="group-title-ratings"><?php echo rah_generate_stars( $group_stats['group_rating'] ); ?></div>
+			  <?php endif; ?>
+			</h1>
+			<!-- .entry-title -->
+			<div class="entry-meta clearfix">
+			  <div class="date"><span>
+				Added On: <?php the_time( get_option( 'date_format' ) ); ?>
+				</span></div>
+			  <div class="by-author"><a href="<?php the_permalink(); ?>">
+				<?php printf( _n( '%d Host', '%d Hosts', $host_count, 'interface' ), $host_count ); ?>
+			  </a></div>
+			  <?php if( $group_stats['group_reviews'] > 0 ) :?>
+			  <div class="comments"><span>Host Reviews: <?php echo $group_stats['group_reviews']; ?></span></div>
+			  <?php endif; ?>
+			</div>
+			<!-- .entry-meta -->
+		  </header>
+		  <!-- .entry-header -->
+		  <?php } ?>
+		  <div class="entry-content clearfix">
+			<?php the_content(); ?>
+			<h5>Hosts</h5>
+			<?php foreach ( $hosts as $host ) : ?>
+			<?php
+			$host_location = get_post_meta( $host['ID'], '_user_postal_code', true );
+			$location_string = '';
+			if ( ! empty( $host_location ) ) {
+				$state = rah_get_postal_state( $host_location );
+				$location_string .= $state;
+			}
+			?>
+			<div>
+				<div class="archive-avatar">
+				  <a href="<?php echo get_permalink( $host['ID'] ); ?>">
+				  <?php echo get_avatar( get_user_id_from_host_id( $host['ID'] ) ); ?>
+				  </a>
+				  <p>
+					<div><a href="<?php echo get_permalink( $host['ID'] ); ?>"><?php echo $host['post_title']; ?></a></div>
+					<div class="star-ratings-wrapper">
+					  <?php echo rah_generate_stars( get_post_meta( $host['ID'], '_host_rating', true ) ); ?>
+					</div>
+					<?php if ( ! empty( $location_string ) ) : ?>
+					<div class="host-location">
+						<span class="dashicons dashicons-location-alt"></span><?php echo $location_string; ?>
+					</div>
+					<?php endif; ?>
+				  </p>
+				</div>
+			</div>
+			<?php endforeach; ?>
+		  </div>
+		  <?php if(get_the_time( get_option( 'date_format' ) )) { ?>
+		</header>
+		<?php } ?>
 
-          <!-- entry content clearfix -->
+		  <!-- entry content clearfix -->
 
-          <?php if( is_single() ) {
-                    $tag_list = get_the_tag_list( '', __( ' ', 'interface' ) );
+		  <?php if( is_single() ) {
+					$tag_list = get_the_tag_list( '', __( ' ', 'interface' ) );
 
-                  if( !empty( $tag_list ) ) { ?>
-          <footer class="entry-meta clearfix"> <span class="tag-links">
-            <?php
-                      echo $tag_list;?>
-            </span><!-- .tag-links -->
-          </footer>
-          <!-- .entry-meta -->
-          <?php  }
-                   }
-          ?>
-        </article>
-      </section>
-      <!-- .post -->
-      <?php
-            do_action( 'interface_after_post' );
+				  if( !empty( $tag_list ) ) { ?>
+		  <footer class="entry-meta clearfix"> <span class="tag-links">
+			<?php
+					  echo $tag_list;?>
+			</span><!-- .tag-links -->
+		  </footer>
+		  <!-- .entry-meta -->
+		  <?php  }
+				   }
+		  ?>
+		</article>
+	  </section>
+	  <!-- .post -->
+	  <?php
+			do_action( 'interface_after_post' );
 
-          }
-        }
-        else {
-          ?>
-      <h1 class="entry-title">
-        <?php _e( 'No Posts Found.', 'interface' ); ?>
-      </h1>
-      <?php
-         }
-      /**
-       * interface_after_loop_content
-     *
-     * HOOKED_FUNCTION_NAME PRIORITY
-     *
-     * interface_next_previous 5
-     * interface_loop_after 10
-       */
-      do_action( 'interface_after_loop_content' );
+		  }
+		}
+		else {
+		  ?>
+	  <h1 class="entry-title">
+		<?php _e( 'No Posts Found.', 'interface' ); ?>
+	  </h1>
+	  <?php
+		 }
+	  /**
+	   * interface_after_loop_content
+	 *
+	 * HOOKED_FUNCTION_NAME PRIORITY
+	 *
+	 * interface_next_previous 5
+	 * interface_loop_after 10
+	   */
+	  do_action( 'interface_after_loop_content' );
    ?>
 </div>
 <!-- #primary -->
 
 <?php
    /**
-    * interface_after_primary
-    */
+	* interface_after_primary
+	*/
    do_action( 'interface_after_primary' );
 ?>
 <div id="secondary">
--- content-hosts.php
+++ content-hosts.php
@@ -9,140 +9,154 @@
 ?>
 <?php
    /**
-    * interface_before_primary
-    */
+	* interface_before_primary
+	*/
    do_action( 'interface_before_primary' );
 ?>
 <div id="primary" class="no-margin-left">
   <?php
-      /**
-       * interface_before_loop_content
+	  /**
+	   * interface_before_loop_content
 		 *
 		 * HOOKED_FUNCTION_NAME PRIORITY
 		 *
 		 * interface_loop_before 10
-       */
-      do_action( 'interface_before_loop_content' );
+	   */
+	  do_action( 'interface_before_loop_content' );
 
-      /**
-       * interface_loop_content
+	  /**
+	   * interface_loop_content
 		 *
 		 * HOOKED_FUNCTION_NAME PRIORITY
 		 *
 		 * interface_theloop 10
-       */
+	   */
   global $post;
 
   if( have_posts() ) {
-    while( have_posts() ) {
-      the_post();
+	while( have_posts() ) {
+	  the_post();
 
-      $parent = get_post_ancestors( $post );
-      if ( $parent ) {
-        $group_url = get_permalink( $parent[0] );
-        $group_name = get_the_title( $parent[0] );
-        $group_image = get_post_meta( $parent[0], '_rah_group_fb_icon', true );
-      }
-      $group_types = get_the_terms( $post->ID, 'type' );
-      $types = implode( ', ', wp_list_pluck( $group_types, 'name' ) );
-      $review_count = get_post_meta( $post->ID, '_host_review_count', true );
+	  $parent = get_post_ancestors( $post );
+	  if ( $parent ) {
+		$group_url = get_permalink( $parent[0] );
+		$group_name = get_the_title( $parent[0] );
+		$group_image = get_post_meta( $parent[0], '_rah_group_fb_icon', true );
+	  }
+	  $group_types = get_the_terms( $post->ID, 'type' );
+	  $types = implode( ', ', wp_list_pluck( $group_types, 'name' ) );
+	  $review_count = get_post_meta( $post->ID, '_host_review_count', true );
+	$host_location = get_post_meta( $post->ID, '_user_postal_code', true );
+	$location_string = '';
+	if ( ! empty( $host_location ) ) {
+		$city = rah_get_postal_city( $host_location );
+		$state = rah_get_postal_state( $host_location );
 
-      do_action( 'interface_before_post' );
-      ?>
+		if ( ! empty( $city ) ) {
+			$location_string .= $city . ', ';
+		}
 
-      <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-        <?php do_action( 'interface_before_post_header' ); ?>
-        <article>
-          <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
-            <div class="archive-avatar">
-              <?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
-            </div>
-          </a>
-          <header class="entry-header">
-            <?php if (get_the_author() !=''){?>
-            <div class="entry-meta"> <span class="cat-links">
-              <?php the_category(', '); ?>
-              </span><!-- .cat-links -->
-            </div>
-            <?php } ?>
-            <!-- .entry-meta -->
-            <h1 class="entry-title"> <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
-              <?php the_title();?>
-              </a> </h1>
-            <!-- .entry-title -->
-             <?php if (get_the_author() !=''){?>
-            <div class="entry-meta clearfix">
-              <div class="date"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_time() ); ?>">
-                Joined On: <?php the_time( get_option( 'date_format' ) ); ?>
-                </a></div>
-              <?php if ( !empty( $parent ) ) : ?>
-              <div class="group"><?php if( $group_image ) :?><img src="<?php echo $group_image; ?>" /> &nbsp;<?php endif; ?>
-                <a href="<?php echo $group_url; ?>" title-"<?php echo esc_attr( $group_name ); ?>">
-                <?php echo $group_name; ?>
-              </a></div>
-              <?php unset( $parent ); ?>
-            <?php endif; ?>
-              <br />
-            <div class="hosts-ratings-wrapper">
-              <?php echo rah_generate_stars( get_post_meta( $post->ID, '_host_rating', true ) ); ?><br />
-              <?php printf( _n( '%d Review', '%d Reviews', $review_count, 'interface' ), $review_count ); ?>
-            </div>
-            </div>
-            <!-- .entry-meta -->
-          </header>
-          <!-- .entry-header -->
-          <div class="entry-content clearfix">
-            <?php the_excerpt(); ?>
-          </div>
-          <!-- .entry-content -->
-          <footer class="entry-meta clearfix"> <span class="tag-links">
-            <?php $tag_list = get_the_tag_list( '', __( ' ', 'interface' ) );
-                  if(!empty($tag_list)){
-                echo $tag_list;
+		$location_string .= $state;
+	}
+	  do_action( 'interface_before_post' );
+	  ?>
 
-                  }?>
-            </span><!-- .tag-links -->
-            <?php
-                  echo '<a class="readmore" href="' . get_permalink() . 'new" title="'.the_title( '', '', false ).'">'.__( 'Rate Host', 'interface' ).'</a>';
-                  ?>
-          </footer>
-          <!-- .entry-meta -->
-           <?php } else { ?>
-         </header>
-              <?php the_content();
-            } ?>
-        </article>
-      </section>
-      <!-- .post -->
-      <?php
-            do_action( 'interface_after_post' );
+	  <section id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<?php do_action( 'interface_before_post_header' ); ?>
+		<article>
+		  <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
+			<div class="archive-avatar">
+			  <?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?>
+			</div>
+		  </a>
+		  <header class="entry-header">
+			<?php if (get_the_author() !=''){?>
+			<div class="entry-meta"> <span class="cat-links">
+			  <?php the_category(', '); ?>
+			  </span><!-- .cat-links -->
+			</div>
+			<?php } ?>
+			<!-- .entry-meta -->
+			<h1 class="entry-title"> <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute();?>">
+			  <?php the_title();?>
+			  </a> </h1>
+			<!-- .entry-title -->
+			 <?php if (get_the_author() !=''){?>
+			<div class="entry-meta clearfix">
+			<?php if ( ! empty( $location_string ) ) : ?>
+				<div class="location"><span class="dashicons dashicons-location-alt"></span><?php echo $location_string; ?></div>
+			<?php endif; ?>
+			  <div class="date"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( get_the_time() ); ?>">
+				Joined On: <?php the_time( get_option( 'date_format' ) ); ?>
+				</a></div>
+			  <?php if ( !empty( $parent ) ) : ?>
+			  <div class="group"><?php if( $group_image ) :?><img src="<?php echo $group_image; ?>" /> &nbsp;<?php endif; ?>
+				<a href="<?php echo $group_url; ?>" title-"<?php echo esc_attr( $group_name ); ?>">
+				<?php echo $group_name; ?>
+			  </a></div>
+			  <?php unset( $parent ); ?>
+			<?php endif; ?>
+			  <br />
+			<div class="hosts-ratings-wrapper">
+			  <?php echo rah_generate_stars( get_post_meta( $post->ID, '_host_rating', true ) ); ?><br />
+			  <?php printf( _n( '%d Review', '%d Reviews', $review_count, 'interface' ), $review_count ); ?>
+			</div>
+			</div>
+			<!-- .entry-meta -->
+		  </header>
+		  <!-- .entry-header -->
+		  <div class="entry-content clearfix">
+			<?php the_excerpt(); ?>
+		  </div>
+		  <!-- .entry-content -->
+		  <footer class="entry-meta clearfix"> <span class="tag-links">
+			<?php $tag_list = get_the_tag_list( '', __( ' ', 'interface' ) );
+				  if(!empty($tag_list)){
+				echo $tag_list;
 
-          }
-        }
-        else {
-          ?>
-      <h1 class="entry-title">
-        <?php _e( 'No Posts Found.', 'interface' ); ?>
-      </h1>
-      <?php
-         }
-      /**
-       * interface_after_loop_content
+				  }?>
+			</span><!-- .tag-links -->
+			<?php
+				  echo '<a class="readmore" href="' . get_permalink() . 'new" title="'.the_title( '', '', false ).'">'.__( 'Rate Host', 'interface' ).'</a>';
+				  ?>
+		  </footer>
+		  <!-- .entry-meta -->
+		   <?php } else { ?>
+		 </header>
+			  <?php the_content();
+			} ?>
+		</article>
+	  </section>
+	  <!-- .post -->
+	  <?php
+			do_action( 'interface_after_post' );
+
+		  }
+		}
+		else {
+		  ?>
+	  <h1 class="entry-title">
+		<?php _e( 'No Posts Found.', 'interface' ); ?>
+	  </h1>
+	  <?php
+		 }
+	  /**
+	   * interface_after_loop_content
 		 *
 		 * HOOKED_FUNCTION_NAME PRIORITY
 		 *
 		 * interface_next_previous 5
 		 * interface_loop_after 10
-       */
-      do_action( 'interface_after_loop_content' );
+	   */
+	  do_action( 'interface_after_loop_content' );
    ?>
 </div>
 <!-- #primary -->
 
 <?php
    /**
-    * interface_after_primary
-    */
+	* interface_after_primary
+	*/
    do_action( 'interface_after_primary' );
 ?>
 <div id="secondary">
